bf06a39c26828cb5b81c47137f431689a48397de
neerajy0326 <neerajy0326@gmail.com>
2023-08-04 13:36:06 +0530

    updated 1.7

--- backend/settings.py
+++ backend/settings.py
@@ -85,8 +85,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',   
     
 ]
 
