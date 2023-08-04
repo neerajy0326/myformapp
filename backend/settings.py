4021d7ded99286a311d61d27d16c991c2928603a
neerajy0326 <neerajy0326@gmail.com>
2023-08-04 11:32:45 +0530

    upgraded

--- backend/settings.py
+++ backend/settings.py
@@ -185,7 +185,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR,"static")
 ]
-
+STATIC_ROOT = '/home/ubuntu/webapp/myformapp/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
