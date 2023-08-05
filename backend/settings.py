02e98efb4d6d1cdf795bda75f7c59b642317f43d
neerajy0326 <neerajy0326@gmail.com>
2023-08-05 16:40:59 +0530

    asz

--- backend/settings.py
+++ backend/settings.py
@@ -181,7 +181,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = '/home/ubuntu/webapp/myform/staticfiles/'
+STATIC_ROOT = '/home/ubuntu/webapp/myformapp/staticfiles/'
 
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [
