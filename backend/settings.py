6622359e57257839f0c238392a9c0c2f64c5ccd9
neerajy0326 <neerajy0326@gmail.com>
2023-08-06 12:52:00 +0530

    sae

--- backend/settings.py
+++ backend/settings.py
@@ -89,7 +89,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+ 
    
     
 ]
